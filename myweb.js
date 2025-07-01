8a47d8c760194dcaaef5b12e212ddca93da001a3
Sahil Yadav <sy05092005@gmail.com>
2025-07-01 19:59:20 +0530

    Update myweb.js

--- myweb.js
+++ myweb.js
@@ -2,11 +2,13 @@ const project=document.querySelector("#projects");
 const certificates=document.querySelector("#Certificates");
 const skills=document.querySelector("#skills");
 const projecthide=document.querySelector(".project");
+const skillhide=document.querySelector(".skill");
 const pannel3=document.querySelectorAll(".pannel3");
 
 
 project.addEventListener("click",()=>{
     console.log("project was clicked");
+    skillhide.classList.add("hide1");
     projecthide.classList.toggle("hide");
 });
 
@@ -14,14 +16,19 @@ skills.addEventListener("click",()=>{
     console.log("skill was clicked");
     projecthide.classList.add("hide");
 
+    skillhide.classList.toggle("hide1");
+
+
 });
 
 
 certificates.addEventListener("click",()=>{
     console.log("certificates was clicked");
     projecthide.classList.add("hide");
+    skillhide.classList.add("hide1");
     
 });
+
 
 
 
